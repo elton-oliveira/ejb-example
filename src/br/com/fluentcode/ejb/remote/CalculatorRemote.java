49225e85c2a412e415a291bca695b331118596ea
elton-oliveira <elton.pmp@gmail.com>
2013-11-22 23:50:06 -0200

    Create build file

--- src/br/com/fluentcode/ejb/remote/CalculatorRemote.java
+++ src/br/com/fluentcode/ejb/remote/CalculatorRemote.java
@@ -2,13 +2,6 @@ package br.com.fluentcode.ejb.remote;
 
 import javax.ejb.Remote;
 
-/**
- * 
- * TODO Preciso tirar o jboss-client.jar daqui preciso, pois isso não é um client.
- * Na hora de executar vai usar o recursos do container. Mas para compilar preciso ver como
- * resolver com o ant e ivy (adicionar um lib com scope de ...)
- *
- */
 @Remote
 public interface CalculatorRemote {
 	
