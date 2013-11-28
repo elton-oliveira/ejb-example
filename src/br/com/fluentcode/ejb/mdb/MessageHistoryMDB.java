ca9f74a488ee97b3094ebb7519814b42b9ed2e6c
elton-oliveira <elton.pmp@gmail.com>
2013-11-28 21:04:31 -0200

    Persistence.xml invoke jndi datasource

--- src/br/com/fluentcode/ejb/mdb/MessageHistoryMDB.java
+++ src/br/com/fluentcode/ejb/mdb/MessageHistoryMDB.java
@@ -6,16 +6,26 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.fluentcode.ejb.orm.MessageHistory;
 
 @MessageDriven(activationConfig={
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
 		@ActivationConfigProperty(propertyName = "destination", propertyValue = "FluentCodeQueue")})
-public class DataBaseMDB implements MessageListener{
+public class MessageHistoryMDB implements MessageListener{
+	
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public void onMessage(Message message) {
 		TextMessage textMessage = (TextMessage) message;
 		try {
+			MessageHistory history = new MessageHistory();
+			history.setDescription(textMessage.getText());
+			em.persist(history);
 			System.out.println("--> "+textMessage.getText()+" <--");
 		} catch (JMSException e) {
 			e.printStackTrace();
---
+++ src/br/com/fluentcode/ejb/orm/MessageHistory.java
@@ -0,0 +1,35 @@
+package br.com.fluentcode.ejb.orm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 
+ * ORM to test JBosss DataSource configuration
+ *
+ */
+@Entity
+public class MessageHistory {
+	
+	@GeneratedValue
+	@Id
+	private Integer id;
+	
+	private String description;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+}
