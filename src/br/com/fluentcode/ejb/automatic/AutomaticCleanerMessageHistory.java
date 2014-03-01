b6b940b15ba1efa5e7981340fbb61a740a26fb4f
elton-oliveira <elton.pmp@gmail.com>
2014-03-01 11:41:13 -0300

    Persistent timer commet

--- src/br/com/fluentcode/ejb/automatic/AutomaticCleanerMessageHistory.java
+++ src/br/com/fluentcode/ejb/automatic/AutomaticCleanerMessageHistory.java
@@ -8,6 +8,9 @@ import javax.ejb.Startup;
 
 import br.com.fluentcode.ejb.dao.MessageHistoryDAOBean;
 
+
+// To test, change clean method to run every 3 seconds: @Schedule(second =" */3 ", minute ="*", hour ="*")
+
 @Singleton
 @Startup
 public class AutomaticCleanerMessageHistory {
@@ -20,7 +23,12 @@ public class AutomaticCleanerMessageHistory {
 		System.out.println("=============== Singleton created on application startup ===============");
 	}
 	
-	//To test, change to run every 3 seconds: @Schedule(second =" */3 ", minute ="*", hour ="*")
+	/**
+	 * Timers are persistent by default. If the server is shut down or crashes,
+	 * persistent timers are saved and will become active again when the server
+	 * is restarted. If a persistent timer expires while the server is down, the
+	 * container will call the @Timeout method when the server is restarted.
+	 */
 	@Schedule(dayOfWeek="Mon,Wed", hour="8", minute="30", persistent=false)
 	public void clean(){
 		int number = this.messageHistoryDAO.deleteAll();
